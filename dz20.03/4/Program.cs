62d067efb808e9819fb540149512eb40f90d96a2
MotyaAbormotya <76623037+MotyaAbormotya@users.noreply.github.com>
2021-03-20 23:03:08 +0300

    Add files via upload

---
+++ dz20.03/4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            CutLetter(text);
+            Console.ReadKey();
+        }
+
+        public static string CutLetter(string text)
+        {
+           
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetter(text[i]) == true)
+                {
+                    text = text.Remove(i, 1);
+                    i--;
+                }
+            }
+            Console.WriteLine(text);
+            return text;
+
+        }
+    }
+}
